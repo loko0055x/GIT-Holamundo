0403630e645948c8c91a9ebab0fcc55cd5d4a6e5
loko0055x <loko2003elcrack@gmail.com>
2023-12-12 19:07:05 -0500

    Soy de la rama master y ya hice cambios

---
+++ src/principal/Claseprincipal.java
@@ -0,0 +1,12 @@
+ 
+package principal;
+ 
+public class Claseprincipal {
+    
+    
+    public static void main(String[] args) {
+        System.out.println("Hola mundo Desde mi pc Hacia  GIT");
+        
+    }
+    
+}
