e890e6d9ca436de5f16f41e5ed8931c63226e405
loko0055x <loko2003elcrack@gmail.com>
2023-12-12 19:34:28 -0500

    soy la rama lokito y agregue una logica de inicio de session

--- src/principal/Claseprincipal.java
+++ src/principal/Claseprincipal.java
@@ -1,12 +1,48 @@
- 
 package principal;
- 
+
+import java.util.Scanner;
+
 public class Claseprincipal {
-    
-    
+
+    static Scanner s = new Scanner(System.in);
+
     public static void main(String[] args) {
-        System.out.println("Hola mundo Desde mi pc Hacia  GIT");
-        
+        System.out.println("---------------------------");
+        String i = "go";
+        while (!i.equals("salir")) {
+            System.out.println("1)Iniciar session");
+            System.out.println("2)Registrate");
+            System.out.println("Otros)Salir");
+            i = s.next();
+            switch (i) {
+                case "1":
+                    iniciarsecion();
+                    break;
+                case "2":
+                    break;
+                default:
+                    i = "salir";
+                    break;
+            }
+        }
+
+        System.out.println("---------------------------");
     }
-    
+
+    private static final String username = "lokito";
+    private static final String password = "14";
+
+    public static void iniciarsecion() {
+        System.out.println("Ingrese nombre");
+        String user = s.next();
+        System.out.println("Ingrese contra");
+        String pass = s.next();
+        if (user.equalsIgnoreCase(username) && pass.equalsIgnoreCase(password)) {
+            System.out.println("Bienvendio al sistema");
+        } else {
+            System.out.println("No existe");
+        }
+
+    }
+
 }
